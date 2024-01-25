1fbb96933079e1463a3c93e692f113a7531f768f
Brian Wilson <bmwilson93@gmail.com>
2024-01-24 22:44:14 -0700

    added page routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+// Page Imports
+import Home from './pages/Home';
+import SearchResult from './pages/SearchResult';
+import Sets from './pages/Sets';
+import Set from './pages/Set';
+import Card from './pages/Card';
+import NoPage from './pages/NoPage';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/search/:id" element={<SearchResult />}/>
+        <Route path="/sets" element={<Sets />}/>
+        <Route path="/set/:id" element={<Set />}/>
+        <Route path="/card/:id" element={<Card />}/>
+        <Route path="*" element={<NoPage />}/>
+      </Routes>
+    </div>
   )
 }
 
