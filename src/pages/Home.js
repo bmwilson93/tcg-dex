d9d6157856914148cadf45d836ea598f17fd7eea
Brian Wilson <bmwilson93@gmail.com>
2024-01-24 22:51:42 -0700

    added content to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home-container">
+      <div className="title-container">
+        <h1>Pok&eacute;mon TCG Dex</h1>
+        <h3>The Ultimate Pok&eacute;mon Card Database</h3>
+        {/* Put Searchbar Here */}
+      </div>
+
+      {/* Add decorative cards here */}
+    </div>
   )
 }
 
