e0f6efdb1ad21eac99ee88d2309f3686cd4270cd
Brian Wilson <bmwilson93@gmail.com>
2024-01-25 19:07:43 -0700

    added search bar input and form to Searchbar.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const Searchbar = () => {
+
+  const isSmall = small ? 'make-small' : '';
+
   return (
-    <div>Searchbar</div>
+    <div className="searchbar-container">
+      <form
+        onSubmit={(e) => {e.preventDefault(); hanldeSubmit();}}
+      >
+        <input
+          type="text"
+          placeholder="Search for a card"
+          value={search}
+          className={isSmall}
+          onChange={(e) => setSearch(e.target.value)}></input>  
+      </form>  
+    </div>
   )
 }
 
