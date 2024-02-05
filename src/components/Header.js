42858cd44409b653fb8da912eb0c758a23bfb28a
Brian Wilson <bmwilson93@gmail.com>
2024-02-05 02:03:00 -0700

    Added Searchbar to Header.js component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ getCards, setRecentSearch, recentSearch, searchbarState, search, setSearch}) => {
+  const location = useLocation();
+
   return (
-    <div>Header</div>
+    <header>
+        <div className="home-container">
+          <Link to='/' className="header-home-link white-link">
+            <img src={logo} alt="logo" width="40px"/>
+            <span className="header-title">Pok&eacute;mon TCG Dex</span>
+          </Link>
+
+          {/* If not on the home page, dispaly the search bar in the header */}
+          {location.pathname !== '/' 
+          ? <Searchbar 
+              getCards={getCards} 
+              setRecentSearch={setRecentSearch} 
+              recentSearch={recentSearch} 
+              small={true}
+              search={search}
+              setSearch={setSearch}
+            /> 
+          : null}
+        </div>
+
+        <nav>
+          <Link to='/sets' className='set-link white-link'>Browse By Set</Link>
+        </nav>
+    </header>
+
   )
 }
 
