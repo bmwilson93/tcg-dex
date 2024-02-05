5fc63a9b89ea4279cd23b8186fdc8a5e5f3c89e8
Brian Wilson <bmwilson93@gmail.com>
2024-02-05 01:57:39 -0700

    Added submit handler to Searchbar.js

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Searchbar = () => {
+
+  const navigate = useNavigate();
+
+  const searchCards = () => {
+    setRecentSearch(search);
+    getCards(search, 1)
+  }
+
+  const handleSubmit = () => {
+    searchCards();
+    navigate(`/search=/${search}`);
+  }
 
   const isSmall = small ? 'make-small' : '';
 
   return (
     <div className="searchbar-container">
       <form
-        onSubmit={(e) => {e.preventDefault(); hanldeSubmit();}}
+        onSubmit={(e) => {e.preventDefault(); handleSubmit();}}
       >
         <input
           type="text"
