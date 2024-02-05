967c8aabfb275134da726cef1a5440509eac6904
Brian Wilson <bmwilson93@gmail.com>
2024-02-05 02:26:57 -0700

    Added CardList component to SearchResult.js

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import CardList from '../components/CardList'
+import PageNavigation from '../components/PageNavigation'
+import Loading from '../components/Loading'
 
-const SearchResult = () => {
+const SearchResult = ({ cards, currentPage, pages, getCards, recentSearch, cardCount}) => {
   return (
-    <div>SearchResult</div>
+    <div className="search-results-container">
+      <CardList cards={cards}/>
+      
+      {(() => {
+        if (cardCount == -1) {
+          return <Loading />
+        } else if (cardCount == 0) {
+          return <div className="not-found-message">No Cards Found...</div>
+        } else {
+          return <PageNavigation currentPage={currentPage} pages={pages} getCards={getCards} recentSearch={recentSearch}/>
+        }
+      })()}
+
+      
+    </div>
   )
 }
 
