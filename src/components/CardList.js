cc34ce989ddb872ee80cb959d0a8689262faf3ee
Brian Wilson <bmwilson93@gmail.com>
2024-02-05 02:37:02 -0700

    Added list for cards in CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import CardItem from './CardItem'
 
-const CardList = () => {
+const CardList = ({ cards }) => {
+  const navigate = useNavigate();
+
+  // Renders a list of cards that is stored in a cards state the App.js file
+  const listOfCards = cards.map(card => 
+    <li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
+      <CardItem name={card.name} 
+        setName={card.set.name} 
+        avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
+        // tcgAvgSellPrice={card.tcgplayer && card.tcgplayer.prices && card.tcgplayer.prices.holofoil || card.tcgplayer.prices.reverseHolofoil || card.tcgplayer.prices.normal}
+        imgSrc={card.images.small}/>
+    </li>
+  )
+
   return (
-    <div>CardList</div>
+    <div className="card-list-container">
+
+      <ul className="card-list">
+        {listOfCards}
+      </ul>
+
+    </div>
   )
 }
 
