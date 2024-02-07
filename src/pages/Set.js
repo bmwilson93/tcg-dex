ffe55bd071346c0d36f332d8ebc9da469be70f8c
Brian Wilson <bmwilson93@gmail.com>
2024-02-06 20:05:35 -0700

    added details to Set.js page

--- src/pages/Set.js
+++ src/pages/Set.js
@@ -1,8 +1,71 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import CardList from '../components/CardList';
+import Loading from '../components/Loading';
 
-const Set = () => {
+import { fetchData } from '../utils/fetchData';
+
+// import './css/Set.css';
+
+const Set = ({ currentSet, setCurrentSet, cards, setCards }) => {
+  const location = useLocation();
+  const dataOfSet = location.state.set;
+
+  const fetchAllCards = async () => {
+    setCards([]);
+    let allCards = [];
+
+    const url = "https://api.pokemontcg.io/v2/cards?q=set.id:" + dataOfSet.id  + "&orderBy=number"
+
+    let responseData = await fetchData(url);
+
+    if ('error' in responseData) {
+      console.log("found an error");
+    } else {
+      allCards.push(...responseData.data);
+  
+      if (responseData.totalCount > responseData.pageSize) {
+        responseData = await fetchData(url + "&page=2");
+
+        allCards.push(...responseData.data);
+      }
+  
+      setCards(allCards);
+      console.log(allCards.length);
+    }
+  };
+
+  useEffect(() => {
+    if (cards.length > 0) {
+      if (cards[0].set.id !== dataOfSet.id) {
+        fetchAllCards();
+      }
+    } else {
+      fetchAllCards();
+    }
+  }, []);
+
+
   return (
-    <div>Set</div>
+    <div className="set-container">
+      <div className="set-image-container">
+        <img src={dataOfSet.images.logo} alt={dataOfSet.name} width='340px'/>
+      </div>
+
+      <div className="set-name-container">
+        <h1>{dataOfSet.name}</h1>
+      </div>
+
+      <div className="card-list-container2">
+        {cards.length > 0 
+        ? <CardList cards={cards}/>
+        : <Loading />}
+        
+      </div>
+
+      <button className="back-to-top-btn" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>Back to Top</button>
+    </div>
   )
 }
 
