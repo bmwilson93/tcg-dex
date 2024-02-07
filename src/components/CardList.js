50e64eb165a41cf176017d1cc16d3833d8144c89
Brian Wilson <bmwilson93@gmail.com>
2024-02-07 00:58:32 -0700

    added css import

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import CardItem from './CardItem'
+import './css/CardList.css';
 
 const CardList = ({ cards }) => {
   const navigate = useNavigate();
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import './css/Header.css';
 
 import logo from '../assets/Pokecoin.png';
 
