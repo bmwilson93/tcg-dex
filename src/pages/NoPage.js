74a7076548f086a8d70794733f3e0a0ded51d714
Brian Wilson <bmwilson93@gmail.com>
2024-02-06 20:11:58 -0700

    added redirect to Home page in NoPage.js

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import './css/NoPage.css'
 
 const NoPage = () => {
+  let navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/')
+  }, []);
+
+  // Should automatically redirect to the Home page, but returns a page just in case
   return (
-    <div className="no-page-container">
-      <h1>Oh No!</h1>
-      <p>Sorry, looks like this page doesn't exist...</p>
+    <div className="no_page_container">
+      <div>
+        <h1>Oh No!</h1>
+        <p>Sorry, looks like this page doesn't exist...</p>
+      </div>
     </div>
   )
 }
